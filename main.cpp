44a250fe1265c3cdfa733cde559f988b2ff0e849
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2024-11-17 15:49:54 +0000

    Vettori (04)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    srand(time(0)); 
+    int i, p, N, imax, imin, indiceminimo, valoreminimo, indicemassimo, valoremassimo;
+    cout << "Inserisci il numero degli elementi del vettore" << endl;
+    cin >> N;
+    vector <int> V(N);
+    for (i=0; i <= N-1 ; i++){
+        V[i]=rand () % 1001;
+        cout << "Valore:" << V[i] << endl;
+    }
+    p = 0;
+    for (i=0; i <= N-1 ; i++){
+        if ((V[i] % 2) == 0) {
+           p=p+1;
+        }
+    }
+    indiceminimo=0;
+    valoreminimo=V[0];
+    indicemassimo=0;
+    valoremassimo=V[0];
+    for (i=0; i <= N-1 ; i++){
+        if(V[i] > valoreminimo){
+            indiceminimo=i;
+            valoreminimo=V[i];
+        }
+        if(V[i] < valoremassimo){
+            indicemassimo=i;
+            valoremassimo=V[i];
+        }      
+    }
+cout << "Il valore più piccolo contenuto nel vettore ha valore" << valoreminimo << " e posizione" << indiceminimo << endl;
+cout << "Il valore più grande contenuto nel vettore ha valore" << valoremassimo << " e posizione" << indicemassimo << endl;
+
+
+
+
+
+
+
+
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
